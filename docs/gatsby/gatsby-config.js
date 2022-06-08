497f17d0177e16b85aee0ce9f23a2d8e4415835f
Shana Matthews <shana.matthews@coinbase.com>
2022-06-08 10:55:52 -0700

    cleanup work and add quickstat doc

--- docs/gatsby/gatsby-config.js
+++ docs/gatsby/gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
   pathPrefix: "",
   siteMetadata: {
     title: 'Query & Transact Advanced Query APIs (preview)',
-    description: "This document describes the preview APIs for Query and Transact's new Advanced Query APIs. See the [main Query and Transact documentation](https://docs.cloud.coinbase.com/blockchain-infrastructure/docs) for more information about Query and Transact. \n\n These APIs allow greater... TBD SHANA \n\n **API constraints:** \n - Only Ethereum protocol and mainnet (and testnet) networks are supported in this preview version of the APIs. Ethereum L2 chains and other protocols will be supported incrementally. \n - This preview version of the APIs only supports ETH, ERC-20 and ERC-721 tokens. \n   - The Balances and Contracts APIs only return results for ERC-20 and ERC-721 tokens. \n   - In the Transfers APIs, some metadata in the results might be available for ERC-20 and ERC-721 only. \n   - Note that the Contracts APIs are only relevant for the Ethereum ecosystem.",
+    description: "Query & Transact Advanced Query APIs (preview)",
     logoUrl: 'https://raw.githubusercontent.com/open-rpc/design/master/icons/open-rpc-logo-noText/open-rpc-logo-noText%20(PNG)/256x256.png',
     primaryColor: '#3f51b5', //material-ui primary color
     secondaryColor: '#f50057', //material-ui secondary color
@@ -15,16 +15,24 @@ module.exports = {
         ignoreNextPrev: true
       },
       {
-        name: 'API Documentation',
-        link: '/api-documentation'
+        name: 'Advanced Query API documentation',
+        link: '/api-documentation',
+        ignoreNextPrev: true
       },
       {
         name: 'Ethereum documentation',
-        link: '/ethereum-documentation'
+        link: '/ethereum-documentation',
+        ignoreNextPrev: true
       },
       {
         name: 'Sample API calls',
-        link: '/sample-calls'
+        link: '/sample-calls',
+        ignoreNextPrev: true
+      },
+      {
+        name: 'Quickstart',
+        link: '/quickstart',
+        ignoreNextPrev: true
       }
     ],
     footerLinks: [
